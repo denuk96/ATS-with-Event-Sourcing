02120f5eb4b023950881dfaa75ddb084a72eff5a
Denys Taradada <e_den9611@live.com>
2023-08-26 11:52:28 +0200

    refactor: replace counters to application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -8,6 +8,31 @@ class Application < ApplicationRecord
   validates :candidate_name, presence: true
 
   scope :with_active_job, -> { joins(:job).where(jobs: { status: Job.statuses[:active] }) }
+  scope :hired, lambda { |job_id|
+    joins(:events)
+      .where(events: { status: ApplicationRelated::Event.statuses[:hired] })
+      .where(job_id:)
+  }
+  scope :rejected, lambda { |job_id|
+    joins(:events)
+      .where(events: { status: ApplicationRelated::Event.statuses[:rejected] })
+      .where(job_id:)
+  }
+  scope :ongoing, lambda { |job_id|
+    # Since each Application might have multiple events, we determine the application's status by its latest event.
+    # Therefore, we need to skip all except the last one
+    # However, it might be more reasonable to add a 'status' field to the 'applications' table
+    # and update it asynchronously using the Event#after_save callback, similar to how Job#update_metrics! is implemented
+    # and then simply calc by statuses avoiding complex query like below
+    latest_event_subquery = ApplicationRelated::Event.select('DISTINCT ON(application_id) application_id, status')
+                                                      .order('application_id, created_at DESC')
+    left_joins(:events)
+      .joins("LEFT JOIN (#{latest_event_subquery.to_sql}) AS latest_events
+                        ON applications.id = latest_events.application_id")
+      .where('latest_events.application_id IS NULL OR latest_events.status NOT IN (?)',
+             [ApplicationRelated::Event.statuses[:rejected], ApplicationRelated::Event.statuses[:hired]])
+      .where(job_id:)
+  }
 
   after_touch { |event| UpdateJobMetricsJob.perform_async(event.job_id) }
 
--- app/models/job.rb
+++ app/models/job.rb
@@ -11,39 +11,11 @@ class Job < ApplicationRecord
   def update_metrics!
     params = {
       status: events.last&.status || JobRelated::Event.statuses[:deactivated],
-      hired: total_hired,
-      rejected: total_rejected,
-      ongoing: total_ongoing
+      hired: applications.hired(id).count,
+      rejected: applications.rejected(id).count,
+      ongoing: applications.ongoing(id).count
     }
 
     update!(params)
-  end
-
-  def total_hired
-    applications.joins(:events)
-                .where(events: { status: ApplicationRelated::Event.statuses[:hired] })
-                .count
-  end
-
-  def total_rejected
-    applications.joins(:events)
-                .where(events: { status: ApplicationRelated::Event.statuses[:rejected] })
-                .count
-  end
-
-  def total_ongoing
-    # Since each Application might have multiple events, we determine the application's status by its latest event.
-    # Therefore, we need to skip all except the last one
-    # However, it might be more reasonable to add a 'status' field to the 'applications' table
-    # and update it asynchronously using the Event#after_save callback, similar to how Job#update_metrics! is implemented
-    # and then simply calc by statuses avoiding complex query like below
-    latest_event_subquery = ApplicationRelated::Event.select('DISTINCT ON(application_id) application_id, status')
-                                                     .order('application_id, created_at DESC')
-    applications.left_joins(:events)
-                .joins("LEFT JOIN (#{latest_event_subquery.to_sql}) AS latest_events
-                        ON applications.id = latest_events.application_id")
-                .where('latest_events.application_id IS NULL OR latest_events.status NOT IN (?)',
-                       [ApplicationRelated::Event.statuses[:rejected], ApplicationRelated::Event.statuses[:hired]])
-                .count
   end
 end
